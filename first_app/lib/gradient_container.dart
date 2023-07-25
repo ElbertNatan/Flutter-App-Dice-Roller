0c7100f5f91963df8719634337aa78d75ecb044b
Elbert <elbertnatan@hotmail.com>
2023-07-25 11:40:23 -0300

    - utilizando var para guardar valores de cor e alinhamento  - removendo const por conta das variaveis

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,17 +1,21 @@
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
+var startGradientAlignment = Alignment.topLeft;
+var endGradientAlignment = Alignment.bottomRight;
+var initialGradient = [Colors.deepOrangeAccent, Colors.deepPurpleAccent];
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.deepOrangeAccent, Colors.deepPurpleAccent],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: initialGradient,
+          begin: startGradientAlignment,
+          end: endGradientAlignment,
         ),
       ),
       child: const Center(
