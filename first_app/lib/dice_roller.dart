beb4858d63afb137c4a45ac79628441d28ab4000
Elbert <elbertnatan@hotmail.com>
2023-07-26 09:23:52 -0300

    - colocando randomize do dado

---
+++ first_app/lib/dice_roller.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var imagemInicial = './assets/images/dice-1.png';
+
+  void rolarDado() {
+    setState(() {
+      imagemInicial = './assets/images/dice-${Random().nextInt(6) + 1}.png';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(imagemInicial, width: 200),
+        //const SizedBox(height: 20),
+        TextButton(
+            onPressed: () {
+              rolarDado();
+            },
+            style: TextButton.styleFrom(
+              padding: const EdgeInsets.only(
+                top: 20,
+              ),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(fontSize: 28),
+            ),
+            child: const Text('Rolar Dado'))
+      ],
+    );
+  }
+}
--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,11 +1,10 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:first_app/styled_text.dart';
 import 'package:flutter/material.dart';
 
 const startGradientAlignment = Alignment.topLeft;
 const endGradientAlignment = Alignment.bottomRight;
 const initialGradient = [Colors.deepOrangeAccent, Colors.deepPurpleAccent];
-
-void rolarDado() {}
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
@@ -22,26 +21,8 @@ class GradientContainer extends StatelessWidget {
           end: endGradientAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('./assets/images/dice-1.png', width: 200),
-            //const SizedBox(height: 20),
-            TextButton(
-                onPressed: () {
-                  rolarDado();
-                },
-                style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                  ),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 28),
-                ),
-                child: Text('Rolar Dado'))
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
